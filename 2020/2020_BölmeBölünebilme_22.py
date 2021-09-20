4b69723f631e0800e57a6ed72131d9b5613dc9fe
Hidayet Can Ulubaş <51166040+HidoCan@users.noreply.github.com>
2021-09-20 22:24:03 +0300

    Delete 2020 directory

--- 2020/2020_BölmeBölünebilme_22.py
+++
@@ -1,8 +0,0 @@
-x=input("Kardeş sayısı giriniz:")
-y=input("Albümdeki fotoğraf sayısı giriniz:")
-z=input("Her birinin bulunduğau fotoğraf sayısı:")
-coz1=x*y
-coz2=x*z
-coz3=coz1-coz2
-sonuc=y-coz3
-print("Kardeşlerin birlikte bulundupu fotoğraf sayısı=",sonuc)
